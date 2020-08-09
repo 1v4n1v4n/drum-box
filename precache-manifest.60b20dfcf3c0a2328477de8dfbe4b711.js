a8e801e4b8a44f1a38ca53c3a300f3af9ad63469
Ivan <filipovmajstor@gmail.com>
2020-08-09 14:23:36 -0500

    Updates

---
+++ precache-manifest.60b20dfcf3c0a2328477de8dfbe4b711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa8c91a5a7fb7af26a0702840f6793d",
+    "url": "/drum-box/index.html"
+  },
+  {
+    "revision": "106ad1e9fc50d51a07bc",
+    "url": "/drum-box/static/css/main.1216e0b8.chunk.css"
+  },
+  {
+    "revision": "1f2585e748cf78810738",
+    "url": "/drum-box/static/js/2.92fc675f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/drum-box/static/js/2.92fc675f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106ad1e9fc50d51a07bc",
+    "url": "/drum-box/static/js/main.2987aed4.chunk.js"
+  },
+  {
+    "revision": "a227169d074d3353000a",
+    "url": "/drum-box/static/js/runtime-main.7d6ac63c.js"
+  },
+  {
+    "revision": "79c436b6f58d95283fe373a1fc1781e2",
+    "url": "/drum-box/static/media/cool-background.79c436b6.png"
+  }
+]);
