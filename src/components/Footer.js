af6f22f48ce9ccb307d276e5fc53ceff9dca2a0d
Ivan <filipovmajstor@gmail.com>
2020-08-09 15:14:37 -0500

    small changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 const Footer = () => {
 	return (
 		<Fragment>
-			<a href='https://github.com/1v4n1v4n'>
+			<a id='footer' href='https://github.com/1v4n1v4n'>
 				<span>by 1v4n</span>
 			</a>
 		</Fragment>
